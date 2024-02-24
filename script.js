2d726ad2c4b53ecc03122d607b994028f120e2c4
Hayzed-codes <raheemhorlah@gmail.com>
2024-02-24 15:37:42 +0100

    tutorials

--- script.js
+++ script.js
@@ -3,12 +3,12 @@
 // Global variable : This is a variable that is defined at the top of the page
 // Local variable : These are variables that can not be used outside of where it has been declared.
 
-let i;
+// let i;
 
-for (i = 0; i < 5; i++) {
-    let p = 10
-    console.log(i + p);
-}
+// for (i = 0; i < 5; i++) {
+//     let p = 10
+//     console.log(i + p);
+// }
 
 // console.log(i);
 
@@ -42,3 +42,60 @@ console.log(`${fullName} is a big boy`)
 let a = 2
 a *= 4;
 console.log(a)
+
+
+// AND OPERATOR
+let result;
+let age = 30;
+
+if (age > 20 && age < 30) {
+    result = age + 50;
+}else {
+    result = age;
+}
+console.log(result)
+
+
+// OR OPERATOR
+
+let price = 50;
+let selPrice;
+let gain = 10;
+
+if (price >= 50 || price === 550){
+    selPrice = price + gain;
+}
+else{
+    selPrice = price;
+}
+
+console.log(selPrice)
+
+// NOT OPERATOR
+
+let name = "Hayzed";
+
+if (name !== "Hayzed") {
+    console.log("Why are you lying?");
+}
+else {
+    console.log("That is good");
+}
+
+// LOOP
+
+// While Loop
+
+let num = 5;
+let i = 0;
+while (i < num) {
+    i++;
+    console.log(i);
+}
+
+// FOR LOOP
+
+for (let index = 0; index < num; index++) {
+    
+    console.log(index)
+}
